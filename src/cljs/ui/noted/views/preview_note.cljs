48e0a493759e6522410c4f168816356565ca3704
Jarrett Atkinson <jj_atkinson@att.net>
2019-02-11 22:09:30 -0600

    finish pinned note feature

--- src/cljs/ui/noted/views/preview_note.cljs
+++ src/cljs/ui/noted/views/preview_note.cljs
@@ -8,30 +8,33 @@
     [re-frame.core :as rf]))
 
 (defn preview-note []
-  [:div.preview-note
-   [:div.top-bar
-    [:div.left
-     [:h2.title (<s [:preview-note/title])]
-     [:span.hint (<s [:preview-note/see-insertion-tag])]]
-    [:div.buttons
-     [:input.search
-      {:type     "button"
-       :value    "< Search"
-       :on-click #(e> [:search-view/goto-view])}]
-     [:i.pin.material-icons
-      {:type     "button"
-       :on-click #(e> [:preview-note/pin])}
-      (if (<s [:preview-note/pinned-mode?])
-        "lock"
-        "lock_open")]
-     [:input.edit
-      {:type     "button"
-       :value "Edit >"
-       :on-click #(e> [:preview-note/goto-editor])}]]]
-   (into [:div.tags]
-         (map (fn [t] [:a
-                       {:on-click #(e> [:search-view/goto-view-with-search t])}
-                       (str "#" t)])
-              (<s [:preview-note/tags])))
-   [:div.content {:dangerouslySetInnerHTML
-                  {:__html (<s [:preview-note/content])}}]])
+  (let [not-pinned? (not (<s [:preview-note/pinned-mode?]))]
+    [:div.preview-note
+     [:div.top-bar
+      [:div.left
+       [:h2.title (<s [:preview-note/title])]
+       [:span.hint (<s [:preview-note/see-insertion-tag])]]
+      [:div.buttons
+       (when not-pinned?
+         [:input.search
+          {:type     "button"
+           :value    "< Search"
+           :on-click #(e> [:search-view/goto-view])}])
+       [:i.pin.material-icons
+        {:type     "button"
+         :on-click #(e> [:preview-note/pin])}
+        (if not-pinned?
+          "lock_open"
+          "close")]
+       (when not-pinned?
+         [:input.edit
+          {:type     "button"
+           :value    "Edit >"
+           :on-click #(e> [:preview-note/goto-editor])}])]]
+     (into [:div.tags]
+           (map (fn [t] [:a
+                         {:on-click #(e> [:search-view/goto-view-with-search t])}
+                         (str "#" t)])
+                (<s [:preview-note/tags])))
+     [:div.content {:dangerouslySetInnerHTML
+                    {:__html (<s [:preview-note/content])}}]]))
